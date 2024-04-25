ba8d2e7f0883f0acc3cec82ac410c9ba07eebe98
Bhanu1132 <kb7097@gmail.com>
2024-04-25 10:44:20 +0530

    added servlet and jsp

--- WordCounter-MavenProject/src/main/java/com/kb/service/WordCounterServlet.java
+++ WordCounter-MavenProject/src/main/java/com/kb/service/WordCounterServlet.java
@@ -1,6 +1,28 @@
 package com.kb.service;
 
-public class WordCounterServlet  
-{
+import java.io.IOException;
+import java.util.StringTokenizer;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/para")
+public class WordCounterServlet extends HttpServlet  
+{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res)
+	throws ServletException, IOException
+	{
+		String para = req.getParameter("para");
+		StringTokenizer st = new StringTokenizer(para, " ");
+		int tokens = st.countTokens();
+		req.setAttribute("token", tokens);
+		RequestDispatcher rd = req.getRequestDispatcher("paraJsp.jsp");
+		rd.forward(req, res);
+	}
 }
